301ee84bbc2ce0d9bd42f0079e148cfb3304995d
Deividy Pinheiro <deividy@gmail.com>
2018-07-24 18:51:45 -0300

    Adicionado serviço de Lançamento

---
+++ src/main/java/com/proximus/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -0,0 +1,46 @@
+package com.proximus.pontointeligente.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.proximus.pontointeligente.api.entities.Lancamento;
+import com.proximus.pontointeligente.api.repositories.LancamentoRepository;
+import com.proximus.pontointeligente.api.services.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService{
+	
+	private static final Logger log = LoggerFactory.getLogger(LancamentoServiceImpl.class);
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	public Page<Lancamento> buscaPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
+		log.info("Buscando lançamentos para o funcionário ID {}", funcionarioId);
+		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+	}
+
+	public Optional<Lancamento> buscarPorId(Long id) {
+		log.info("Buscando um lançamento ID {}", id);
+		return Optional.ofNullable(this.lancamentoRepository.findById(id).orElse(null));
+	}
+
+	public Lancamento persistir(Lancamento lancamento) {
+		log.info("Persistindo o lançamento: {}", lancamento);
+		return this.lancamentoRepository.save(lancamento);
+	}
+
+	public void remover(Long id) {
+		log.info("Removendo o lançamento: {}", id);
+		this.lancamentoRepository.deleteById(id);
+		
+	}
+	
+	
+}
